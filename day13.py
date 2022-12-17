1f9fa2ea1e4b732945a7ed50ea2a53f7e3f7fa4d
Boris <bfleyshe@ualberta.ca>
2022-12-16 23:45:38 -0700

    Day 13 part 2

--- day13.py
+++ day13.py
@@ -1,9 +1,9 @@
 # Day 13
 
 def isSorted(lhs, rhs):
-	print()
-	print(lhs)
-	print(rhs)
+	#print()
+	#print(lhs)
+	#print(rhs)
 
 	# both inputs are int
 	if type(lhs) == int and type(rhs) == int:
@@ -45,26 +45,55 @@ def isSorted(lhs, rhs):
 
 
 sortedIndicies = []
+sortedList = []
+sortedList.append([[2]])
+sortedList.append([[6]])
+
 with open('input.txt', 'r') as file:
 	i = 0
 	j = 1
 	for line in file:
 
-		if i % 3 == 0:
-			left = eval(line)
-		if i % 3 == 1:
-			right = eval(line)
-		if i % 3 == 2:
-			sorted = isSorted(left, right)
-			if sorted >= 0:
-				sorted = True
-			else:
-				sorted = False
-			print(sorted)
-			if sorted:
-				sortedIndicies.append(j)
-			j += 1
-		i += 1
+		#Q2
+		for i in range(len(sortedList)):
+			if line == "\n":
+				continue
+			newElement = eval(line)
+			ele = sortedList[i]
+			inOrder = isSorted(ele, newElement)
+			if inOrder == -1:
+				sortedList.insert(i, newElement)
+				break
+
+		# #Q1
+		# if i % 3 == 0:
+		# 	left = eval(line)
+		# if i % 3 == 1:
+		# 	right = eval(line)
+		# if i % 3 == 2:
+		# 	sorted = isSorted(left, right)
+		# 	if sorted >= 0:
+		# 		sorted = True
+		# 	else:
+		# 		sorted = False
+		# 	print(sorted)
+		# 	if sorted:
+		# 		sortedIndicies.append(j)
+		# 	j += 1
+		# i += 1
 
 print("Sorted: ", sortedIndicies)
 print(sum(sortedIndicies))
+
+indicies = []
+for i in range(len(sortedList)):
+	print(sortedList[i])
+for i in range(len(sortedList)):
+	if sortedList[i] == [[2]] or sortedList[i] == [[6]]:
+		print(i+1)
+		indicies.append(i+1)
+
+total = 1
+for ele in indicies:
+	total *= ele
+print(total)
