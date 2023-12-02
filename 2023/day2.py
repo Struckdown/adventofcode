01236b2d8b29b02b6fa937051b0f7d1356dfafce
Boris <bfleyshe@ualberta.ca>
2023-12-01 23:35:35 -0700

    2023 2b

--- 2023/day2.py
+++ 2023/day2.py
@@ -1,10 +1,12 @@
 def solve():
 	f = open("input.txt", "r")
 	sum = 0
+	powerSum = 0
 
 	limit = {"red":12, "green":13, "blue":14}
 
 	for line in f:
+		minimum = {"red":0, "green":0, "blue":0}
 		gameNum = line.split(":")[0].split()[1]
 		gameNum = int(gameNum)
 		cubes = line.split(":")[1]
@@ -20,15 +22,24 @@ def solve():
 				currentSet[color] = count
 				if count > limit[color]:
 					valid = False
-					break
+					#break	# we can't early break for part 2
+				minimum[color] = max(minimum[color], count)
 
 		# if valid, we can add to our sum
 		if (valid):
 			sum += gameNum
 
+		# for part 2, we grab the cube power
+		localPower = 1
+		for color in minimum:
+			localPower *= minimum[color]
+		powerSum += localPower
+		print(minimum, localPower)
+
 
 	# output the answer
 	print(sum)
+	print(powerSum)
 
 if __name__ == '__main__':
 	solve()
